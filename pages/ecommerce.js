6307231cb033d0fad19b2b5617aa414fef477024
anovakovsky <anovakovsky@admins-iMac.local>
2018-12-19 15:49:24 +0200

    Add block BUG FIXING, fixed particle.js - script work only ID element i change for Class andrepair component ParallaxText

--- pages/ecommerce.js
+++ pages/ecommerce.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
-import ParallaxText from "../components/e-commerce/ParallaxText";
+import ParallaxText from "../components/ParallaxText";
 import Page from "../components/Page";
 import Rate from "../components/e-commerce/Rate";
 import ThemeDev from "../components/e-commerce/ThemeDev";
 import Brands from "../components/e-commerce/Brands";
 import Platform from "../components/e-commerce/platform/Platform";
+import BugFixing from "../components/e-commerce/BugFixing";
 
 export default class Ecommerce extends Component {
     render() {
         return (
             <Page>
-                <ParallaxText/>
+                <ParallaxText stickyParent='posts'>e-commerce</ParallaxText>
+                <div id='posts'/>
                 <Rate/>
                 <ThemeDev/>
                 <Brands/>
                 <Platform/>
+                <BugFixing/>
             </Page>
         )
     }
