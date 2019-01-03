efa15f163c9072b0681e99985517743782fc4a86
Olha <olha.volovyk@skynix.co>
2019-01-03 10:21:51 +0200

    SKRC-55 Skynix technology stack(Create new animation)

--- components/technologies/TitleHeader.js
+++ components/technologies/TitleHeader.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Animated} from "react-animated-css";
 import Particle from "../Particle";
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TextLoop from "react-text-loop";
 
 export default function TitleHeader() {
     const animation = {
         animationIn: 'fadeInDown',
         animationInDelay: 400
     };
-    return (
-    <Animated {...animation}>
-        <div className='ec-titleheader'>
-            <h1 className='heading ec-titleheader__heading'><b>skynix</b> technology stack</h1>
-            <h2 className='heading__sub heading__sub-line'>
-                    <span className='heading__sub-item'>hands-on experience</span>
-                    <span className='heading__sub-item'>latest technologies </span>
-                    <span className='heading__sub-item'>proven frameworks</span>
-                    <span className='heading__sub-item'>all time favorite platforms</span>
-            </h2>
-        </div>
-    </Animated>
-    )
-}
+    return(
+        <Animated {...animation}>
+            <div className='ec-titleheader'>
+                <h1 className='heading ec-titleheader__heading'><b>skynix</b> technology stack</h1>
+                <h2 className='heading__sub'>
+                    <TextLoop>
+                            <span className='heading__sub-item'>hands-on experience</span>
+                            <span className='heading__sub-item'>latest technologies </span>
+                            <span className='heading__sub-item'>proven frameworks</span>
+                            <span className='heading__sub-item'>all time favorite platforms</span>
+                    </TextLoop>
+                </h2>
+            </div>
+        </Animated>
+        )
+    }
