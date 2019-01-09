9c792844e26909810e02bc69bc43c7a4d3d890bc
anovakovsky <anovakovsky@admins-iMac.local>
2019-01-09 13:58:18 +0200

    SKRC-66 creat page about skynix

---
+++ components/about/TitleHeader.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Animated} from "react-animated-css";
+
+export default function TitleHeader() {
+    const animation = {
+        animationIn: 'fadeInDown',
+        animationInDelay: 400
+    };
+    return(
+        <Animated {...animation}>
+            <div className='ec-titleheader'>
+                <h1 className='heading ec-titleheader__heading'>about <b>skynix</b></h1>
+            </div>
+        </Animated>
+        )
+    }
