f35fb286ad16d74a0a6473dfff7b6746015c0c35
anovakovsky <anovakovsky@admins-iMac.local>
2019-01-09 14:58:42 +0200

    SKRC-67 add block history

--- pages/about-skynix.js
+++ pages/about-skynix.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Page from "../components/Page";
 import TitleHeader from "../components/about/TitleHeader";
+import History from "../components/about/History";
 
 export default class AboutSkynix extends Component {
     render() {
         return (
             <Page>
                 <TitleHeader/>
+                <History/>
             </Page>
         )
     }
