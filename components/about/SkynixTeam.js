78111d1d8c2b3747b7eae64698d7489b14bf235e
anovakovsky <anovakovsky@admins-iMac.local>
2019-01-10 17:33:10 +0200

    SKRC-69 add block team

--- components/about/SkynixTeam.js
+++ components/about/SkynixTeam.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import LazyLoad from "../LazyLoad";
+import PropTypes from 'prop-types';
+import OwlCarousel from 'react-owl-carousel2';
+import SectionTeams from "./libs/SectionTeams.js";
+import values from './configs/section-teams.config.json';
 
-export default function SkynixTeam() {
-	function SectionTeams(props) {
-		return <div className="as-skynixteam__col">
-					<img src={props.imageTeam} alt={props.nameTeam} />
-					<h4 className="as-skynixteam__name">{props.nameTeam}</h4>
-					<p className="as-skynixteam__position">{props.positionTeam}</p>
-					<p className="as-skynixteam__description">{props.shortBioTeam}</p>
-					<ul className="as-skynixteam__social">
-						<li><a target="_blank" href={props.facebookLinksTeam}></a></li>
-						<li><a target="_blank" href={props.linkedinLinksTeam}></a></li>
-						<li><a target="_blank" href={props.skypeLinksTeam}></a></li>
-					</ul>
-		</div>
-	}
+export default function SkynixTeam(props) {
+
+	const options = {
+		loop:false,
+		margin:20,
+		autoplayHoverPause:true,
+		autoplayTimeout:3000,
+		nav:true,
+		autoplay: false,
+		responsive: {
+			0:{
+				items:1,
+			},
+			544:{
+				items:2,
+			},
+			1024:{
+				items:3,
+			},
+		}
+	};
+
+	let team = values.map(value => <SectionTeams {...value} key={value.id} />);
+
 	return (
 		<LazyLoad className='as-skynixteam'>
-			<h2 className='heading'>skynix <b>team</b></h2>
-			<div className="row">
-				<SectionTeams nameTeam='Bogdan Kushlyk' imageTeam='https://s3.amazonaws.com/s3-wp-staging/wp-content/uploads/2018/04/22162438/bogdan-k.jpg' positionTeam='Business Development Manager' shortBioTeam='Strong skills in planning, market analysis, and project management. Proven as an effective communicator able to make different teams co-work.' facebookLinksTeam='https://www.facebook.com/SkynixLLC/' linkedinLinksTeam='https://www.linkedin.com/company/skynix/' skypeLinksTeam='https://www.linkedin.com/company/skynix/'/>
-			</div>
+			<h2 className='heading as-skynixteam__heading'>skynix <b>team</b></h2>
+			<OwlCarousel options={options}>
+				{team}
+			</OwlCarousel>
 		</LazyLoad>
 	)
 }
+
+SkynixTeam.propTypes = {
+	idTeam: PropTypes.number.isRequired,
+	imageTeam: PropTypes.string,
+	nameTeam: PropTypes.string,
+	positionTeam: PropTypes.string,
+	shortBioTeam:  PropTypes.string,
+	facebookLinksTeam:  PropTypes.string,
+	linkedinLinksTeam:  PropTypes.string,
+	skypeLinksTeam:  PropTypes.string
+};
