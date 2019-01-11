4e63a3c340ca5a32ad03afe4cfbd0aecc3889cb5
Olha <olha.volovyk@skynix.co>
2019-01-11 12:25:30 +0200

    SKRC-56 Rich backend(Change brands view)

--- components/e-commerce/BrandItem.js
+++ components/e-commerce/BrandItem.js
@@ -3,19 +3,10 @@ import { Animated } from "react-animated-css";
 import PropTypes from 'prop-types';
 
 export default function BrandItem(props) {
-    const {children, show, img, imgClass, id, link, linkTitle, name} = props;
+    const {show, img, imgClass, id, link, linkTitle} = props;
     return (
         <Animated className='ec-brands__item' isVisible={show} animationInDelay={id * 250}>
-            <img src={'/static/images/brands/' + img} className={imgClass}/>
-            { children ?
-                <div className="ec-brands__item-wrap">
-                    <ul>
-                        { children.map((item) => 
-                            <li>{item.name}</li>)
-                        }
-                    </ul>
-                </div>
-            : null }  
+            <img src={'/static/images/brands/' + img} className={imgClass}/> 
             { link
                 ?   <a href={link}>
                         <span>More about starting with {linkTitle}</span>
@@ -28,12 +19,10 @@ export default function BrandItem(props) {
 }
 
 BrandItem.propTypes = {
-    children: PropTypes.array,
     show: PropTypes.bool,
     img: PropTypes.string,
     imgClass: PropTypes.string,
     id: PropTypes.number.isRequired,
     link: PropTypes.string,
-    linkTitle: PropTypes.string,
-    name: PropTypes.string
+    linkTitle: PropTypes.string
 }
