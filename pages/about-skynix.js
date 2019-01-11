f9a8344fb66572e3760d308597dd9c44b6ca2f60
anovakovsky <anovakovsky@admins-iMac.local>
2019-01-11 13:15:34 +0200

    SKRC-70 creat block careers (without form), now use futer form

--- pages/about-skynix.js
+++ pages/about-skynix.js
@@ -3,6 +3,8 @@ import Page from "../components/Page";
 import TitleHeader from "../components/about/TitleHeader";
 import History from "../components/about/History";
 import Ideology from "../components/about/Ideology";
+import Careers from "../components/about/Careers";
+import Modal from "../components/about/libs/Modal.js";
 
 export default class AboutSkynix extends Component {
     render() {
@@ -11,6 +13,8 @@ export default class AboutSkynix extends Component {
                 <TitleHeader/>
                 <History/>
                 <Ideology/>
+                <Careers/>
+                <Modal/>
             </Page>
         )
     }
