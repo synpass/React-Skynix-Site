5db461856255e568d5ddf74d52bd1d1bcda01614
Olha <olha.volovyk@skynix.co>
2019-01-15 16:58:38 +0200

    SKRC-34 Cliens (Create block)

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Page from "../components/Page";
 import Experience from "../components/portfolio/Experience";
+import Clients from "../components/portfolio/Clients";
 
 export default function Portfolio(props) {
 	return (
         <Page newsItems={props.newsItems}>
             <Experience/>
+            <Clients/>
         </Page>
 	)
 }
