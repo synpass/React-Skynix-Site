9fa80d89f8c7bcb8c29288a27aacb570c654ed12
Nastya <nastyakachura@gmail.com>
2019-01-15 15:51:34 +0200

    SKRC-121 removed typo

--- pages/technologies.js
+++ pages/technologies.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Page from "../components/Page";
 import TitleHeader from "../components/technologies/TitleHeader";
 import RichBackend from "../components/technologies/RichBackend";
-import ElegantFrontend from "../components/technologies/ElegantFrontend";git git
+import ElegantFrontend from "../components/technologies/ElegantFrontend";
 import meta from './index-meta.config.json';
 import QualityAssurance from "../components/technologies/QualityAssurance";
 import Engine from "../components/technologies/Engine";
