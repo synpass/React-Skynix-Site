e35a0d267909f5c3265099f5fd67bc79e48e019b
avystorobskiu <avystorobskiu@iMac-Admin-3.local>
2019-01-23 12:09:41 +0200

    Added gtmId variable and GTM tracking id

--- domain.config.js
+++ domain.config.js
@@ -1,8 +1,19 @@
 let hostname = 'https://<hostName>';
 let cmsHost = 'https://<cmsHost>';
+let gtmId;
 
+    if (hostname == 'https://skynix.co') {
+    
+        gtmId = 'GTM-MV4FKFF'
+    
+    }else{
+    
+        gtmId = 'GTM-N6CK5NF'
+    
+    }
 
-export {cmsHost}; 
+export {cmsHost} 
+export {gtmId}
 
 export default hostname
 
