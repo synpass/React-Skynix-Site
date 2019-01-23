5826a88692bc40ea16e34e20e24339466783d12f
avystorobskiu <avystorobskiu@iMac-Admin-3.local>
2019-01-23 18:19:29 +0200

    SCKRC-84 New form backend variable

--- domain.config.js
+++ domain.config.js
@@ -1,17 +1,7 @@
 let hostname = 'https://<hostName>'; //production: skynix.co, staging: staging.skynix.co
 let cmsHost = 'https://<cmsHost>'; //production: cms.skynix.co, staging: staging.cms.skynix.co
 let gtmId = '<gtmId>'; // production: GTM-MV4FKFF, staging: GTM-N6CK5NF
-
-hostname = 'https://staging.skynix.co'
-cmsHost = 'https://staging.cms.skynix.co'
-
-let contactFormId;
-
-if (hostname == 'https://skynix.co') {
-    contactFormId = '1841'
-}else if (hostname == 'https://staging.skynix.co') {
-    contactFormId = '1252'
-}
+let contactFormId = "<contactFormId>";
 
 export {contactFormId}
 export {cmsHost} 
