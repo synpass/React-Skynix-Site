6243372f1a572292d73228b1532e307a8fa622b6
avystorobskiu <avystorobskiu@iMac-Admin-3.local>
2019-01-23 16:26:45 +0200

    SKRC-84 Added dynamic formId to config

--- domain.config.js
+++ domain.config.js
@@ -2,7 +2,18 @@ let hostname = 'https://<hostName>'; //production: skynix.co, staging: staging.s
 let cmsHost = 'https://<cmsHost>'; //production: cms.skynix.co, staging: staging.cms.skynix.co
 let gtmId = '<gtmId>'; // production: GTM-MV4FKFF, staging: GTM-N6CK5NF
 
+hostname = 'https://staging.skynix.co'
+cmsHost = 'https://staging.cms.skynix.co'
 
+let contactFormid;
+
+if (hostname == 'https://skynix.co') {
+    contactFormid = '1252'
+}else if (hostname == 'https://staging.skynix.co') {
+    contactFormid = '1841'
+}
+
+export {contactFormid}
 export {cmsHost} 
 export {gtmId}
 
