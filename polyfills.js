816fd6520ea8de03de3d0bbacd4a5c8cc716a632
Nastya <nastyakachura@gmail.com>
2019-03-05 14:51:13 +0200

    SKRC-190 fixed errors in chrome 41

---
+++ polyfills.js
@@ -0,0 +1,9 @@
+import includes from "core-js/library/fn/string/virtual/includes";
+import repeat from "core-js/library/fn/string/virtual/repeat";
+import assign from "core-js/library/fn/object/assign";
+import values from "core-js/library/fn/object/values";
+
+String.prototype.includes = includes;
+String.prototype.repeat = repeat;
+Object.assign = assign;
+Object.values = values;
