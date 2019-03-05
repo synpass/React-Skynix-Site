b0dab132d7834fe781b7b24a7d4c40942a700ec7
ssmovzh <ssmovzh@iMac-Admin-3.local>
2019-03-05 10:53:11 +0200

    Update blog article styling

--- components/article-page/ArticleBody.js
+++ components/article-page/ArticleBody.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import AuthorPic from "./AuthorPic";
 export default function ArticleBody(props) {
     const {content} = props;
-
     return (
         <div className='blog-article__content-text'>
             {ReactHtmlParser(content.rendered)}
