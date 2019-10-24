18c685bec061cb9006becc826dcd30aaec55acb9
arthur <arthur@admins-iMac-2.local>
2019-10-24 15:56:16 +0300

    SKRC-219 Portflio page

--- domain.config.js
+++ domain.config.js
@@ -1,5 +1,5 @@
 let hostname = 'https://<hostName>'; //production: skynix.co, staging: staging.skynix.co
-let cmsHost = 'https://<cmsHost>'; //production: cms.skynix.co, staging: staging.cms.skynix.co
+let cmsHost = 'https://cms.skynix.co'; //production: cms.skynix.co, staging: staging.cms.skynix.co
 let gtmId = '<gtmId>'; // production: GTM-MV4FKFF, staging: GTM-N6CK5NF
 let contactFormId = "<contactFormId>";
 
