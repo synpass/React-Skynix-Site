6f322b9f87dbbfd557067c09c9b6c69c2c2a56fc
askrypka <askrypka@iPhone.skynix.local>
2019-11-06 14:21:27 +0200

    SKRC-226 footer add clutch badge

---
+++ components/footer/ClutchBadge.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ClutchBadge() {
+    return (
+        <div className='m-footer__clutch-badge'>
+            <a href="https://skynix.co/resources/skynix-llc-is-one-of-the-best-in-ukraine"> <img src='/static/images/Clutch-badge.png' alt='Clutch Badge'/> </a>
+        </div>
+    )
+}
--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import News from "./News";
 import Social from "./Social";
+import ClutchBadge from "./ClutchBadge";
 import Nav from "./Nav";
 import {CookieBanner} from '@palmabit/react-cookie-law';
 
@@ -20,6 +21,7 @@ export default function Footer(props) {
                     <div className='m-footer__col m-footer__col--right'>
                         <h4 className='m-footer__title'>Skynix Social</h4>
                         <Social/>
+                        <ClutchBadge/>
                         <span className='m-footer__copy'>Skynix LLC 2018</span>
                     </div>
                 </div>
